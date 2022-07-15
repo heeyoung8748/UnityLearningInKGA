0b34479c5055744013add94111d662c45c6ed66d
kwonheeyoung <heeyoung8748@gmail.com>
2022-07-15 17:42:39 +0900

    220715#UniRun
    1. 캐릭터 및 배경 설정
    2. collider 설정
    3. 오디오 삽입
    4. 이미지 분할하고 애니메이션 생성
    5. 유한 상태 머신 이론

---
+++ Uni-Run/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+
+public class PlayerController : MonoBehaviour 
+{
+   public AudioClip deathClip;
+   public float jumpForce = 700f;
+
+   private int jumpCount = 0; 
+   private bool isGrounded = false;
+   private bool isDead = false; 
+
+   private Rigidbody2D _rigidbody; 
+   private Animator _animator;
+   private AudioSource _audioSource;
+
+   private void Start() 
+   {
+       _rigidbody = GetComponent<Rigidbody2D>();
+       _animator = GetComponent<Animator>();
+       _audioSource = GetComponent<AudioSource>();
+   }
+
+   private void Update() 
+   {
+       
+   }
+
+   private void Die() 
+   {
+       
+   }
+
+   private void OnTriggerEnter2D(Collider2D other) {
+       // 트리거 콜라이더를 가진 장애물과의 충돌을 감지
+   }
+
+   private void OnCollisionEnter2D(Collision2D collision) {
+       // 바닥에 닿았음을 감지하는 처리
+   }
+
+   private void OnCollisionExit2D(Collision2D collision) {
+       // 바닥에서 벗어났음을 감지하는 처리
+   }
+}
